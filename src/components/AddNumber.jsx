f75e24ed96a4299ffb1835144108626f52566bc1
Hyewon Joo <joo7875@gmail.com>
2020-02-03 17:48:12 -0800

    Added components folder

---
+++ src/components/AddNumber.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+export default class AddNumber extends Component {
+	state = {size:1}
+
+	onChange = (e) => {
+		this.setState({size: Number(e.target.value)});
+	}
+	onClick = (e) => {
+		this.props.onClick(this.state.size);
+	}
+  
+  render() {
+    return (
+      <div>
+        <h1>Add Number</h1>
+        <input type='button' value='+' onClick={this.onClick.bind(this)}></input>
+        <input type='text' value={this.state.size} onChange={this.onChange.bind(this)}></input>
+      </div>
+    )
+  }
+}
---
+++ src/components/AddNumberRoot.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import AddNumber from "../components/AddNumber";
+
+export default class AddNumberRoot extends Component {
+
+  onClick = (size) => {
+    this.props.onClick(size);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Number Root</h1>
+        <AddNumber onClick={this.onClick.bind(this)}></AddNumber>
+      </div>
+    );
+  }
+}
